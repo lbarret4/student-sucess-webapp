9c9649a9713a184d73e46a1089026f0c5af2925f
Richard Garner <richardgarner@mac.com>
2018-10-09 21:36:28 -0500

    wrote a function to restrict useres who are too young to register

--- src/client/components/admin/Register.tsx
+++ src/client/components/admin/Register.tsx
@@ -16,29 +16,47 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
             email: '',
             github: '',
             password: '',
+            today: 0,
         };
     }
 
     private registering = false;
+
+    componentWillMount() {
+        this.setState({ today: Date.now() }) //sets this.today = to today's date in milliseconds passed since january 1st 1970
+    }
+
+    calucalteTime() {
+        let birthday = Date.parse(`${this.state.dob}`); //turns the provided date of birth into a number of milliseocds that have passed since january 1st 1970
+        let today = this.state.today; // today's date in milliseconds, as previously stated
+        let result = (today - birthday) / 31540000000; // today's date in milliseconds, minus the user's birthday in milliseconds, divided by the total number of milliseconds in one year, (not accounting for leap years)
+        return result // returns a number to be measured against the number 21, the minumum age requiered to register with the site.
+    }
 
     RegisterUser = async (e: React.FormEvent<HTMLFormElement>) => {
 
         e.preventDefault();
 
         if (this.registering) return;
-        this.registering = true;
-        try {
-            let token = await json('/auth/register',
-                'POST',
-                this.state
-            );
-            SetAccessToken(token);
-            this.props.history.push('/');
-        } catch (e) {
-            console.log(e);
-        } finally {
-            this.registering = false;
+        let ageConfirmation = this.calucalteTime();
+        if (ageConfirmation >= 21) {
+            this.registering = true;
+            try {
+                let token = await json('/auth/register',
+                    'POST',
+                    this.state
+                );
+                SetAccessToken(token);
+                this.props.history.push('/');
+            } catch (e) {
+                console.log(e);
+            } finally {
+                this.registering = false;
+            }
+        } else {
+            alert("You are too young to register here.")
         }
+
     }
 
     render() {
@@ -115,4 +133,5 @@ interface IRegisterState {
     email: string;
     github: string;
     password: string;
+    today: number;
 }
