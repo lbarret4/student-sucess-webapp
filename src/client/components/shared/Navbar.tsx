5465dab99bc3ebdd75e7dc01a4012ba5609d8db6
Richard Garner <richardgarner@mac.com>
2018-10-10 17:36:22 -0500

    navbar prepared to greet user once logged in

--- src/client/components/shared/Navbar.tsx
+++ src/client/components/shared/Navbar.tsx
@@ -9,7 +9,7 @@ export default class Navbar extends React.Component {
 
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-                <Link to="/" className="navbar-brand" href="#">Covalence Student Success App</Link>
+                { isLoggedIn()? <Link to="/dashboard" className="navbar-brand" href="#"> Welcome, "User Name Here"!</Link>:<Link to="/" className="navbar-brand" href="#">Covalence Student Success App</Link> }
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
