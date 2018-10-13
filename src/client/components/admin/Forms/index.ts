9af612c4fa99966417fef4b07ba106631724bcb1
lbarret4 <llewellynbar4@gmail.com>
2018-10-13 19:44:54 -0400

    export forms components

---
+++ src/client/components/admin/Forms/index.ts
@@ -0,0 +1,6 @@
+import { ApplicationForms } from "./ApplicationForms";
+import { InterviewForms } from "./InterviewForms";
+import { NetworkForms } from "./NetworkForms";
+import { SummaryForms } from "./SummaryForms";
+
+export{ApplicationForms,InterviewForms,NetworkForms,SummaryForms};
