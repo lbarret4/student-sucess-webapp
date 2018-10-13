6cc3e076d99b0d5791d68f8926ec2e14b5e634fe
lbarret4 <llewellynbar4@gmail.com>
2018-10-13 00:10:35 -0400

    Added forms route

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './public/Home';
 import Navbar from './shared/Navbar';
@@ -10,20 +10,22 @@ import Compose from './admin/Compose';
 import Footer from './shared/Footer';
 import PrivateRoute from './admin/privateRoute';
 import Logout from './admin/Logout';
+import InputForms from './admin/Forms/InputForms';
 
 export default class App extends React.Component {
     render() {
         return (
             <Router>
                 <>
-                <Navbar></Navbar>
-                <Route exact path="/" component={Home} />
-                <Route path="/blog/:blogId" component={Blog} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/logout" component={Logout} />
-                <Route exact path="/register" component={Register} />
-                <PrivateRoute exact path="/admin" component={Compose} />
-                <Footer></Footer>
+                    <Navbar></Navbar>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/blog/:blogId" component={Blog} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/logout" component={Logout} />
+                    <Route exact path="/register" component={Register} />
+                    <Route  path="/forms" component={InputForms} />
+                    <PrivateRoute exact path="/admin" component={Compose} />
+                    <Footer></Footer>
                 </>
             </Router>
         );
