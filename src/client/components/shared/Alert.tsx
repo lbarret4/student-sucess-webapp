ffe09fc577f871e366a0b2118b1a5a6c7ac79c8d
lbarret4 <llewellynbar4@gmail.com>
2018-10-14 19:10:13 -0400

    made displaying close button optional

--- src/client/components/shared/Alert.tsx
+++ src/client/components/shared/Alert.tsx
@@ -5,12 +5,13 @@ const MSG_TYPE_CLASS = ['alert-danger', 'alert-info', 'alert-success']
 export default class Alert extends React.Component<IAlertProps>{
 
     render() {
+        let closeBtn:IAlertProps["closeBtn"] = this.props.closeBtn || false;
         return (
             <div className={`alert ${MSG_TYPE_CLASS[this.props.messageType]} alert-dismissible fade show`} role="alert">
                 {this.props.message}
-                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+              { closeBtn? <button type="button" className="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                </button>
+                </button> :null}             
             </div>
         );
     }
@@ -19,6 +20,7 @@ export default class Alert extends React.Component<IAlertProps>{
 export interface IAlertProps {
     message: string;
     messageType: MessageTypes;
+    closeBtn?:boolean;
 }
 
 export enum MessageTypes {
