2fe8d3aeea6e508557b811928a4da863aa63113e
lbarret4 <llewellynbar4@gmail.com>
2018-10-14 20:53:15 -0400

    Changed routes for Interview and Application

--- src/client/components/admin/InputForms.tsx
+++ src/client/components/admin/InputForms.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Route } from 'react-router'
 import { RouteComponentProps } from 'react-router-dom';
 import NavTabs from './Forms/NavTabs';
-import { NetworkForms, InterviewForms, ApplicationForms, SummaryForms } from "./Forms";
+import { NetworkForms,SummaryForms, Interview, Application } from "./Forms";
 
 export default class InputForms extends React.Component<RouteComponentProps> {
 
@@ -14,8 +14,8 @@ export default class InputForms extends React.Component<RouteComponentProps> {
                     <div className="card text-center" >
                         <NavTabs formTabs={[...formTabs]} />
                         <Route exact path={`${this.props.match.path}`} component={NetworkForms} />
-                        <Route exact path={`${this.props.match.path}/Interview`} component={InterviewForms} />
-                        <Route exact path={`${this.props.match.path}/Application`} component={ApplicationForms} />
+                        <Route  path={`${this.props.match.path}/Interview`} component={Interview} />
+                        <Route  path={`${this.props.match.path}/Application`} component={Application} />
                         <Route exact path={`${this.props.match.path}/Summary`} component={SummaryForms} />
                     </div >
                 </div>
