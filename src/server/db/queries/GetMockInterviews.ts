14e5b15586063a510469775a254218b5da02ffb1
Michael Stringer <mstringer09@yahoo.com>
2018-10-16 11:27:45 -0500

    updated query

--- src/server/db/queries/GetMockInterviews.ts
+++ src/server/db/queries/GetMockInterviews.ts
@@ -1,17 +1,21 @@
 import pool from '../pool';
 
-export default () => {
+export default (userid?: any) => {
     return new Promise<IGetMockInterview>((resolve, reject) => {
         pool.query(`
         Select
-            u.first_name, u.last_name, s.service_type, cs._created
-        From career_services cs
-            join services s on s.service_type = cs.service_type
-            join users u on u.userid = cs.userid
-            WHERE s.service_type = "Mock Interview"
-            `,
+            cs.service_type
+        From
+	        career_services cs
+        join
+            users u on u.userid = cs.userid
+        WHERE
+            cs.service_type = 4
+        and 
+            u.userid = ?;"
+            `, userid, 
             (err, results) => {
-                if(err) {
+                if (err) {
                     reject(err)
                 } else {
                     resolve(results)
@@ -21,8 +25,5 @@ export default () => {
 }
 
 export interface IGetMockInterview {
-    first_name?: string;
-    last_name?: string;
     service_type?: number;
-    _created?: Date;
 };
