a94b3328d5d2e61bb5c6e61e876d1d37a4c096e8
Michael Stringer <mstringer09@yahoo.com>
2018-10-17 13:18:38 -0500

    added routes to file

---
+++ src/server/routes/api/applications.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+
+import TableRouter from 'tablerouter';
+import { IApplications, Applications} from '../../db';
+
+const isAdmin: RequestHandler = (req, res, next) => {
+
+    if(!req.user || req.user.role !== 'admin' || req.user.role !== 'guest') {
+        return res.sendStatus(401);
+    }
+
+    return next();
+}
+
+export default new TableRouter<IApplications>(Applications, {
+    canDelete: isAdmin,
+    canWrite: isAdmin,
+    canRead: isAdmin
+}).Router
