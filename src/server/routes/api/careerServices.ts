49ff494e08f40d5e53eb5c9ec4dd36bcf2288c1c
Michael Stringer <mstringer09@yahoo.com>
2018-10-17 13:06:07 -0500

    added guest abilities

--- src/server/routes/api/careerServices.ts
+++ src/server/routes/api/careerServices.ts
@@ -1,4 +1,19 @@
+import { RequestHandler } from 'express';
+
 import TableRouter from 'tablerouter';
 import { ICareerServices, CareerServices} from '../../db';
 
-export default new TableRouter<ICareerServices>(CareerServices, {}/*empty object is where u add privileges ex: canWrite, canDelete is Admin*/).Router
+const isAdmin: RequestHandler = (req, res, next) => {
+
+    if(!req.user || req.user.role !== 'admin' || req.user.role !== 'guest') {
+        return res.sendStatus(401);
+    }
+
+    return next();
+}
+
+export default new TableRouter<ICareerServices>(CareerServices, {
+    canDelete: isAdmin,
+    canWrite: isAdmin,
+    canRead: isAdmin
+}/*empty object is where u add privileges ex: canWrite, canDelete is Admin*/).Router
