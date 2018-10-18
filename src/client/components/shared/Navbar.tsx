44a66de6c9cf9b8a8cd3c2d9c80120dfc519fee4
Richard Garner <richardgarner@mac.com>
2018-10-18 14:00:51 -0500

    Forms path brought up to speed with previous commit

--- src/client/components/shared/Navbar.tsx
+++ src/client/components/shared/Navbar.tsx
@@ -40,10 +40,13 @@ export default class Navbar extends React.Component<any, INavState> {
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item active">
-                            {isLoggedIn() ? <Link to="/dashboard" className="nav-link" href="#">My Dashboard <span className="sr-only">(current)</span></Link> : <div></div>}
+                            {isLoggedIn() ? <Link to="/forms" className="nav-link" href="#">Schedual Events <span className="sr-only">(current)</span></Link> : <div></div>}
                         </li>
                         <li className="nav-item active">
                             {isLoggedIn() ? <Link to="/calender" className="nav-link" href="#">My Calender <span className="sr-only">(current)</span></Link> : <div></div>}
+                        </li>
+                        <li className="nav-item active">
+                            {isLoggedIn() ? <Link to="/dashboard" className="nav-link" href="#">My Dashboard <span className="sr-only">(current)</span></Link> : <div></div>}
                         </li>
                         <li className="nav-item active">
                             {isLoggedIn() ? <div></div> : <Link to="/" className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>}
