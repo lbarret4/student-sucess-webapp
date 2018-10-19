ff54884950bd345687dba2a3a79233d379efc085
lbarret4 <llewellynbar4@gmail.com>
2018-10-19 12:58:16 -0400

    update to post in useractivities table and clear textarea after submit

--- src/client/components/admin/Compose.tsx
+++ src/client/components/admin/Compose.tsx
@@ -15,6 +15,7 @@ export default class Compose extends React.Component<IComposeProps, IComposeStat
         this.state = {
             body: '',
             saveStatus: null
+
         }
     }
 
@@ -22,24 +23,33 @@ export default class Compose extends React.Component<IComposeProps, IComposeStat
     private saving: boolean = false;
 
     SaveBlog = async (e: React.FormEvent<HTMLFormElement>) => {
-        
+
         e.preventDefault(); //don't do a form submission since we call an api
 
-        if(this.saving) return;
+        if (this.saving) return;
 
         try {
             this.saving = true;
             let result = await json('/api/jobactivities', 'POST', {
                 activity_content: this.state.body
             });
-            if(result) {
-                this.setState({saveStatus: 'success' })
+
+            let results2 = await json(`/api/useractivites/`, 'POST', {
+                userid: User.userid,
+                activityid: await result.id
+            });
+            if (result) {
+                this.setState({ 
+                    saveStatus: 'success',
+                    body:''
+             });
+         
             } else {
-                this.setState({saveStatus: 'error' })
+                this.setState({ saveStatus: 'error' })
             }
-        } catch(e) {
+        } catch (e) {
             console.log(e);
-            this.setState({saveStatus: 'error' });
+            this.setState({ saveStatus: 'error' });
         } finally {
             this.saving = false;
         }
@@ -47,7 +57,7 @@ export default class Compose extends React.Component<IComposeProps, IComposeStat
 
     render() {
 
-        if(!this.state.saveStatus) {
+        if (!this.state.saveStatus) {
             this.alert = null;
         } else {
             this.alert = this.state.saveStatus === 'success' ? ALERT_SUCCESS : ALERT_ERROR;
@@ -55,17 +65,17 @@ export default class Compose extends React.Component<IComposeProps, IComposeStat
 
         return (
             <main className="py-5">
-                 <div className="container ">
+                <div className="container ">
                     <div className="row">
                         <div className="col-md-4 offset-md-4">
                             {alert}
                         </div>
                     </div>
                     <div className="row">
-                        <form className="col-md-6 offset-md-3" onSubmit={ this.SaveBlog }>
+                        <form className="col-md-6 offset-md-3" onSubmit={this.SaveBlog}>
                             <div className="form-row">
                                 <div className="col form-group">
-                                    <textarea className="form-control" placeholder="Description of Activity" onChange={ (e) => { this.setState({body: e.target.value } )} } required />
+                                    <textarea className="form-control" placeholder="Description of Activity" onChange={(e) => { this.setState({ body: e.target.value }) }} value={this.state.body} required />
                                 </div>
                             </div>
                             <div className="form-row form-group">
@@ -75,8 +85,8 @@ export default class Compose extends React.Component<IComposeProps, IComposeStat
                             </div>
                         </form>
                     </div>
-                 </div>
-             </main>
+                </div>
+            </main>
         );
     }
 }
