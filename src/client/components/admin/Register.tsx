fe3d3c1eab0d559b4eede48db6e1a3cbb63256e9
Richard Garner <richardgarner@mac.com>
2018-10-19 15:00:57 -0500

    attempting to register users

--- src/client/components/admin/Register.tsx
+++ src/client/components/admin/Register.tsx
@@ -9,14 +9,16 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
     constructor(props: IRegisterProps) {
         super(props);
         this.state = {
-            firstname: '',
-            lastname: '',
-            dob: '',
-            city: '',
-            usstate: '',
-            email: '',
+            user: {
+                firstname: '',
+                lastname: '',
+                dob: '',
+                city: '',
+                usstate: '',
+                email: '',
+                password: '',
+            },
             github: '',
-            password: '',
             today: 0,
             tooYoung: false,
         };
@@ -29,7 +31,7 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
     }
 
     calucalteTime() {
-        let birthday = Date.parse(`${this.state.dob}`); //turns the provided date of birth into a number of milliseocds that have passed since january 1st 1970
+        let birthday = Date.parse(`${this.state.user.dob}`); //turns the provided date of birth into a number of milliseocds that have passed since january 1st 1970
         let today = this.state.today; // today's date in milliseconds, as previously stated
         let result = (today - birthday) / 31540000000; // today's date in milliseconds, minus the user's birthday in milliseconds, divided by the total number of milliseconds in one year, (not accounting for leap years)
         return result // returns a number to be measured against the number 21, the minumum age requiered to register with the site.
@@ -46,9 +48,13 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
             try {
                 let token = await json('/auth/register',
                     'POST',
-                    this.state
+                    this.state.user
                 );
                 SetAccessToken(token);
+                await json(`/api/github/`, 'POST', {
+                    userid: token.userid,
+                    github_link: this.state.github
+                })
                 this.props.history.push('/dashboard');
             } catch (e) {
                 console.log(e);
@@ -56,7 +62,7 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
                 this.registering = false;
             }
         } else {
-            this.setState({tooYoung: true})
+            this.setState({ tooYoung: true })
         }
 
     }
@@ -64,7 +70,7 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
     render() {
 
         let alert;
-        if(this.state.tooYoung) {
+        if (this.state.tooYoung) {
             alert = <Alert message="You are too young to use this site. The minimum requierment is 21 years of age." messageType={MessageTypes.Error}></Alert>;
         } else {
             alert = null;
@@ -72,82 +78,147 @@ export default class Register extends React.Component<IRegisterProps, IRegisterS
 
         return (
             <main className="py-5">
-                    <div className="container py-5">
+                <div className="container py-5">
                     <div className="row">
                         <div className="col-md-4 offset-md-4">
                             {alert}
                         </div>
                     </div>
-                        <form className="row" onSubmit={this.RegisterUser}>
-                            <div className="col-md-4 offset-md-4">
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="First Name" onChange={(e) => { this.setState({ firstname: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="Last Name" onChange={(e) => { this.setState({ lastname: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="Date of Birth (mm/dd/yyyy)" onChange={(e) => { this.setState({ dob: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="City" onChange={(e) => { this.setState({ city: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="State ID (AL, TX, TN, ...)" onChange={(e) => { this.setState({ usstate: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="GitHub Username" onChange={(e) => { this.setState({ github: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="text" className="form-control" placeholder="Email" onChange={(e) => { this.setState({ email: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col form-group">
-                                        <input type="password" className="form-control" placeholder="Password" onChange={(e) => { this.setState({ password: e.target.value }) }} required />
-                                    </div>
-                                </div>
-                                <div className="form-row form-group">
-                                    <div className="col">
-                                        <button className="btn btn-primary btn-lg w-100">Register</button>
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col">
-                                        Already have an account? <Link to="/login">Log in!</Link>
-                                    </div>
+                    <form className="row" onSubmit={this.RegisterUser}>
+                        <div className="col-md-4 offset-md-4">
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="First Name" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.firstname;
+                                        this.setState({
+                                            user: {
+                                                firstname: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                            {console.log(this.state.user.firstname)}
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="Last Name" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.lastname;
+                                        this.setState({
+                                            user: {
+                                                lastname: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="Date of Birth (mm/dd/yyyy)" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.dob;
+                                        this.setState({
+                                            user: {
+                                                dob: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="City" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.city;
+                                        this.setState({
+                                            user: {
+                                                city: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="State ID (AL, TX, TN, ...)" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.usstate;
+                                        this.setState({
+                                            user: {
+                                                usstate: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="GitHub Username" onChange={(e) => { this.setState({ github: e.target.value }) }} required />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="text" className="form-control" placeholder="Email" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.email;
+                                        this.setState({
+                                            user: {
+                                                email: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col form-group">
+                                    <input type="password" className="form-control" placeholder="Password" onChange={(e) => {
+                                            let other = JSON.parse(JSON.stringify(this.state.user));
+                                            delete other.password;
+                                        this.setState({
+                                            user: {
+                                                password: e.target.value,
+                                              ...other
+                                            }                                
+                                            
+                                        })
+                                    }} required />
+                                </div>
+                            </div>
+                            <div className="form-row form-group">
+                                <div className="col">
+                                    <button className="btn btn-primary btn-lg w-100">Register</button>
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="col">
+                                    Already have an account? <Link to="/login">Log in!</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </main>
-                );
-            }
-        }
-        
-interface IRegisterProps extends RouteComponentProps {}
-                interface IRegisterState {
-                    firstname: string;
-                lastname: string;
-                dob: string;
-                city: string;
-                usstate: string;
-                email: string;
-                github: string;
-                password: string;
-                today: number;
-                tooYoung: boolean
-            }
+        );
+    }
+}
+
+interface IRegisterProps extends RouteComponentProps { }
+interface IRegisterState {
+    user: { firstname: string, lastname: string, dob: string, city: string, usstate: string, email: string, password: string }
+    github: string;
+    today: number;
+    tooYoung: boolean
+}
