88e98b2cc78fe837d74ffda8aca54d4a0f8bdc6e
lbarret4 <llewellynbar4@gmail.com>
2018-10-23 00:58:42 -0400

    route for repo commit number

---
+++ src/server/routes/api/repo.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+
+import { getUserCommitNum } from '../../utils/github';
+
+const router = express.Router();
+
+router.get('/:username', async (req, res, next) => {
+    
+    const username = req.params.username;
+    console.log(username);
+    try {
+        let  github = await getUserCommitNum(username);
+        res.json(await github);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+    
+});
+
+export default router;
