065c54bda156c42c99bc674e752feb187581ca2a
lbarret4 <llewellynbar4@gmail.com>
2018-10-24 00:43:46 -0400

    add getNumBlogWeekly

--- src/client/components/admin/WStats.tsx
+++ src/client/components/admin/WStats.tsx
@@ -24,7 +24,7 @@ export default class WStats extends React.Component<IWStatsProps, IWStatsState> 
         this.state = {
             commits: '',
             Start: /* moment().format('YYYY-MM-DD') */'2018-10-10',
-            End: /* moment().add(1, 'w').format('YYYY-MM-DD') */'2018-10-17',
+            End: /* moment().add(1, 'w').format('YYYY-MM-DD') */'2018-10-24',
             posts: '',
             jobsapps: '',
             netactivites: '',
@@ -37,25 +37,54 @@ export default class WStats extends React.Component<IWStatsProps, IWStatsState> 
         try {
             let Start = this.state.Start;
             let End = this.state.End;
-            console.log(Start);
-            console.log(End);
+            let id = User.userid;
+            let data = await json(`/api/blogs/find`, 'POST', { userid: id });
+            let linkBlog = await data[0]["heroku_link"];
 
-            let [WeeklyStats1, WeeklyStats2, WeeklyStats3, WeeklyStats4, WeeklyStats5, WeeklyStats6] = await Promise.all(
-                [json(`/api/q/commitnumber/${User.userid}/${Start}/${End}`),
-                json(`/api/q/numberintweek/${User.userid}/${Start}/${End}`),
-                json(`/api/q/numberjobapps/${User.userid}/${Start}/${End}`),
-                json(`/api/q/numbermockint/${User.userid}/${Start}/${End}`),
-                json(`/api/q/numbernetact/${User.userid}/${Start}/${End}`),
-                json(`https://testing-boiler.herokuapp.com/api/blogs`)
-                ])
-             
-            console.log(WeeklyStats1, WeeklyStats2, WeeklyStats3, WeeklyStats4, WeeklyStats5, WeeklyStats6);
-             this.setState({ commits: WeeklyStats1[0].number_commits, jobsapps: WeeklyStats3[0].count, netactivites: WeeklyStats5[0].activitycount, mockinterviews: WeeklyStats4[0].NumMockInt, interviews: WeeklyStats2[0].NumberOfInterviews, posts: WeeklyStats6.length});
+            let [
+                WeeklyStats1,
+                WeeklyStats2,
+                WeeklyStats3,
+                WeeklyStats4,
+                WeeklyStats5,
+                blogs
+            ] = await Promise.all(
+                [
+                    json(`/api/q/numbercommitwk/${id}/${Start}/${End}`),
+                    json(`/api/q/numberintweek/${id}/${Start}/${End}`),
+                    json(`/api/q/numberjobapps/${id}/${Start}/${End}`),
+                    json(`/api/q/numbermockint/${id}/${Start}/${End}`),
+                    json(`/api/q/numbernetact/${id}/${Start}/${End}`),
+                    json(`${await linkBlog}`)
+                ]);
+            let WeeklyStats6 = await this.getNumBlogWeekly(blogs, '2018-10-04', '2018-10-22');
+            this.setState(
+                {
+                    commits: WeeklyStats1[0].NumsofCommitsaWk,
+                    jobsapps: WeeklyStats3[0].count,
+                    netactivites: WeeklyStats5[0].activitycount,
+                    mockinterviews: WeeklyStats4[0].NumMockInt,
+                    interviews: WeeklyStats2[0].NumberOfInterviews,
+                    posts: WeeklyStats6
+                });
         } catch (e) {
             throw (e);
         }
     }
 
+    async getNumBlogWeekly(blogs: any, start: any, end: any) {
+        try {
+            let publishedBlogs = await blogs.filter((blog: any) => {
+                let publishedTime = blog["__created"];
+                let hasPublishedThisWeek = moment(publishedTime).isBetween(start, end, 'day', '[]');
+                return hasPublishedThisWeek;
+            });
+            return publishedBlogs.length;
+        } catch (error) {
+            throw error;
+        }
+
+    }
 
     render() {
         return (
@@ -69,7 +98,7 @@ export default class WStats extends React.Component<IWStatsProps, IWStatsState> 
                     <div className="card mb-3" style={{ maxWidth: "22rem" }}>
                         <ul className="list-group list-group-flush">
                             <li className="list-group-item">Commits this week</li>
-                            <li className="list-group-item">Most recent blog post</li>
+                            <li className="list-group-item">Blog posts this week</li>
                             <li className="list-group-item">Job applications this week</li>
                             <li className="list-group-item">Networking activities attended this week</li>
                             <li className="list-group-item">Mock interviews this week</li>
@@ -79,11 +108,11 @@ export default class WStats extends React.Component<IWStatsProps, IWStatsState> 
 
                     <div className="card mb-3" style={{ maxWidth: "22rem" }}>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">{this.state.commits}</li>   
+                            <li className="list-group-item">{this.state.commits}</li>
                             <li className="list-group-item">{this.state.posts}</li>
                             <li className="list-group-item">{this.state.jobsapps}</li>
                             <li className="list-group-item">{this.state.netactivites}</li>
-                            <li className="list-group-item">{this.state.mockinterviews}</li> 
+                            <li className="list-group-item">{this.state.mockinterviews}</li>
                             <li className="list-group-item">{this.state.interviews}</li>
                         </ul>
                     </div>
