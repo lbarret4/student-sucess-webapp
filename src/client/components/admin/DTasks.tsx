bcae8736bd6624e9aee2b890aa1c055914311a52
Richard Garner <richardgarner@mac.com>
2018-10-25 21:57:53 -0500

    skeleton for remembered checkboxes created

--- src/client/components/admin/DTasks.tsx
+++ src/client/components/admin/DTasks.tsx
@@ -1,7 +1,70 @@
 
 import * as React from 'react';
 
-export default class DTasks extends React.Component<any, any> {
+export default class DTasks extends React.Component<any, IDTasks> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            dtask1: true,
+            dtask2: false,
+            dtask3: false,
+            date: 0
+        };
+    }
+
+    storeIt() {
+      // stores all state values to database
+    }
+
+    d1True() {
+        return this.state.dtask1
+    }
+    d2True() {
+        return this.state.dtask2
+    }
+    d3True() {
+        return this.state.dtask3
+    }
+
+    dTask1() {
+        if (this.state.dtask1) {
+            this.setState({ dtask1: false })
+        } else {
+            this.setState({ dtask1: true })
+        }
+        this.storeIt()
+    }
+    dTask2() {
+        if (this.state.dtask2) {
+            this.setState({ dtask2: false })
+        } else {
+            this.setState({ dtask2: true })
+        }
+        this.storeIt()
+    }
+    dTask3() {
+        if (this.state.dtask3) {
+            this.setState({ dtask3: false })
+        } else {
+            this.setState({ dtask3: true })
+        }
+        this.storeIt()
+    }
+
+    componentWillMount() {
+        // get database values for checkboxes and date they were last stored
+        let d = new Date()
+        let date = d.getDate()
+        if (date != this.state.date) {
+                this.setState({
+                    date,
+                    dtask1: false,
+                    dtask2: false
+                })
+                this.storeIt()
+        }
+    }
 
     render() {
         return (
@@ -10,15 +73,15 @@ export default class DTasks extends React.Component<any, any> {
                     <h5 className="card-header">Daily Tasks</h5>
                     <div className="card-body">
                         <div>
-                            <input type="checkbox" id="dtask1" />
+                            {this.d1True() ? <input type="checkbox" id="dtask1" checked onChange={(e) => { this.dTask1() }} /> : <input type="checkbox" id="dtask1" onChange={(e) => { this.dTask1() }} />}
                             <label htmlFor="dtask1"> Class assignments</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="dtask2" />
+                        {this.d2True() ? <input type="checkbox" id="dtask2" checked onChange={(e) => { this.dTask2() }} /> : <input type="checkbox" id="dtask1" onChange={(e) => { this.dTask2() }} />}
                             <label htmlFor="dtask2"> Commit to GitHub</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="dtask3" />
+                        {this.d3True() ? <input type="checkbox" id="dtask3" checked onChange={(e) => { this.dTask3() }} /> : <input type="checkbox" id="dtask1" onChange={(e) => { this.dTask3() }} />}
                             <label htmlFor="dtask3">Make contact with Career Coach</label>
                         </div>
                     </div>
@@ -26,4 +89,11 @@ export default class DTasks extends React.Component<any, any> {
             </React.Fragment>
         );
     }
+}
+
+interface IDTasks {
+    dtask1: boolean,
+    dtask2: boolean,
+    dtask3: boolean,
+    date: number
 }
--- src/client/components/admin/WTasks.tsx
+++ src/client/components/admin/WTasks.tsx
@@ -1,26 +1,87 @@
 
 import * as React from 'react';
 import json from '../../utils/api';
+import { number } from 'prop-types';
 
-export default class WTasks extends React.Component<any, any> {
+export default class WTasks extends React.Component<any, IWTasks> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            wtask1: true,
+            wtask2: false,
+            date: 28
+        };
+    }
+
+    storeIt() {
+        // stores all state values to database
+    }
+
+    w1True() {
+        return this.state.wtask1
+    }
+    w2True() {
+        return this.state.wtask2
+    }
+
+    wTask1() {
+        if (this.state.wtask1) {
+            this.setState({ wtask1: false })
+        } else {
+            this.setState({ wtask1: true })
+        }
+        this.storeIt()
+    }
+    wTask2() {
+        if (this.state.wtask2) {
+            this.setState({ wtask2: false })
+        } else {
+            this.setState({ wtask2: true })
+        }
+        this.storeIt()
+    }
+
+    componentWillMount() {
+        // get database values for checkboxes and date they were last stored
+        let d = new Date()
+        let day = d.getDay()
+        let date = d.getDate()
+        if (day === 0) {
+            if (date != this.state.date) {
+                this.setState({
+                    date,
+                    wtask1: false,
+                    wtask2: false
+                })
+                this.storeIt()
+            }
+        }
+    }
 
     render() {
         return (
             <React.Fragment>
                 <div className="card task mb-3 shadow">
-                            <h5 className="card-header">Weekly Tasks</h5>
-                            <div className="card-body">
-                                <div>
-                                    <input type="checkbox" id="wtask1" />
-                                    <label htmlFor="wtask1">Submit Three Applications</label>
-                                </div>
-                                <div>
-                                    <input type="checkbox" id="wtask2" />
-                                    <label htmlFor="wtask2">Post you your Developer Blog</label>
-                                </div>
-                            </div>
+                    <h5 className="card-header">Weekly Tasks</h5>
+                    <div className="card-body">
+                        <div>
+                            {this.w1True() ? <input type="checkbox" id="wtask1" checked onChange={(e) => { this.wTask1() }} /> : <input type="checkbox" id="wtask1" onChange={(e) => { this.wTask1() }} />}
+                            <label htmlFor="wtask1">Submit Three Applications</label>
                         </div>
+                        <div>
+                            {this.w1True() ? <input type="checkbox" id="wtask2" checked onChange={(e) => { this.wTask2() }} /> : <input type="checkbox" id="wtask2" onChange={(e) => { this.wTask2() }} />}
+                            <label htmlFor="wtask2">Post you your Developer Blog</label>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
+}
+
+interface IWTasks {
+    wtask1: boolean,
+    wtask2: boolean,
+    date: number
 }
