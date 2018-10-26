3f7155d33975155d4a4ebf7522e9709d232379c9
Richard Garner <richardgarner@mac.com>
2018-10-25 21:07:25 -0500

    home screen redirects to dashboard when logged in

--- src/client/components/public/Home.tsx
+++ src/client/components/public/Home.tsx
@@ -7,13 +7,8 @@ export default class Home extends React.Component<any, IHomeState>{
         return (
             <main className="py-5">
                 {isLoggedIn() ?
-                    <div className="container py-5" style={{ marginLeft: "200px" }}>
-                        <h1 className="text-center">Welcome to the Covalence Student Success App!</h1>
-                        <div className="text-center">Using this app, you'll be able to schedual interviews, meeting, and many other things.</div>
-                        <div className="text-center">Use your GitHub user ID to track how many commits you've made each week.</div>
-                        <div className="text-center">Display your most recent blog post and stay on track with daily and weekly check lists!</div>
-                        <div className="text-center">Take control of your Covalence Experiance and set yourself up for succsess!</div>
-                        <div className="text-center">Get started by <Link to="/register">registering</Link> or by loging in through the Navbar!</div>
+                    <div>
+                        {this.props.history.push('/dashboard')}
                     </div>
                     :
                     <div className="container py-5">
