4661162323d43075cfc31561978e9086dc6e8e8f
Michael Stringer <mstringer09@yahoo.com>
2018-10-26 16:48:59 -0500

    fixed typos

--- src/client/components/public/Home.tsx
+++ src/client/components/public/Home.tsx
@@ -9,20 +9,20 @@ export default class Home extends React.Component<any, IHomeState>{
                 {isLoggedIn() ?
                     <div className="container py-5" style={{ marginLeft: "200px" }}>
                         <h1 className="text-center">Welcome to the Covalence Student Success App!</h1>
-                        <div className="text-center">Using this app, you'll be able to schedual interviews, meeting, and many other things.</div>
-                        <div className="text-center">Use your GitHub user ID to track how many commits you've made each week.</div>
-                        <div className="text-center">Display your most recent blog post and stay on track with daily and weekly check lists!</div>
-                        <div className="text-center">Take control of your Covalence Experiance and set yourself up for succsess!</div>
-                        <div className="text-center">Get started by <Link to="/register">registering</Link> or by loging in through the Navbar!</div>
+                        <div className="text-center">Using this app, you'll be able to schedule interviews, meetings, and many other things.</div>
+                        <div className="text-center">Use your GitHub user ID to track how many commits you have made each week.</div>
+                        <div className="text-center">Display your most recent blog post and stay on track with daily and weekly checklists!</div>
+                        <div className="text-center">Take control of your Covalence Experience and set yourself up for success!</div>
+                        <div className="text-center">Get started by <Link to="/register">registering</Link> or by logging in through the Navbar!</div>
                     </div>
                     :
                     <div className="container py-5">
                         <h1 className="text-center">Welcome to the Covalence Student Success App!</h1>
-                        <div className="text-center">Using this app, you'll be able to schedual interviews, meeting, and many other things.</div>
-                        <div className="text-center">Use your GitHub user ID to track how many commits you've made each week.</div>
-                        <div className="text-center">Display your most recent blog post and stay on track with daily and weekly check lists!</div>
-                        <div className="text-center">Take control of your Covalence Experiance and set yourself up for succsess!</div>
-                        <div className="text-center">Get started by <Link to="/register">registering</Link> or by loging in through the Navbar!</div>
+                        <div className="text-center">Using this app, you'll be able to schedule interviews, meetings, and many other things.</div>
+                        <div className="text-center">Use your GitHub user ID to track how many commits you have made each week.</div>
+                        <div className="text-center">Display your most recent blog post and stay on track with daily and weekly checklists!</div>
+                        <div className="text-center">Take control of your Covalence Experience and set yourself up for success!</div>
+                        <div className="text-center">Get started by <Link to="/register">registering</Link> or by logging in through the Navbar!</div>
                     </div>}
 
             </main>
